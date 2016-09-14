6169403138a16fda400e126c8ca4beefca8e6b19
Pieter Breed <pieter.breed@gmail.com>
2016-09-14 09:38:30 +0200

    got the ansible inventory script to read from env vars

--- ansible-playbooks/inventory.clj
+++ ansible-playbooks/inventory.clj
@@ -12,10 +12,10 @@
 
 ;; ----------------------------------------
 
-(let [vagrant-host ["vagrant" [192 168 69 100] 22]] 
+(let [vagrant-host ["vagrant" (env/env :houstan-hostname) 22]] 
   (as-> yinv/empty-inventory $
     (yinv/target-> $ vagrant-host {"ansible_user" "vagrant"
-                                   "ansible_host" "192.168.69.100"})
+                                   "ansible_host" (env/env :houstan-hostname)})
     (yinv/target->group-> $ vagrant-host "vagrant-hosts")
     (yinv/target->group-> $ vagrant-host "java-machines")
     (yinv/inv->ansible--list $)
