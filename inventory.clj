76bb3d71d947790056c944339dec404593ded915
Pieter Breed <pieter.breed@gmail.com>
2016-09-26 16:11:19 +0200

    swapped over to ansible-inventory-clj

--- inventory.clj
+++ inventory.clj
@@ -1,12 +1,12 @@
 #!/usr/bin/env boot
 
-(set-env! :dependencies '[[pieterbreed/yoostan-lib "0.0.1-SNAPSHOT"]
+(set-env! :dependencies '[[pieterbreed/ansible-inventory-clj "0.1.1"]
                           [environ "1.0.3"]
                           [org.clojure/data.json "0.2.6"]])
 
 ;; ----------------------------------------
 
-(require '[yoostan-lib.inventory :as yinv])
+(require '[ansible-inventory-clj.core :as yinv])
 (require '[environ.core :as env])
 (require '[clojure.data.json :as json])
 
@@ -14,10 +14,10 @@
 
 (let [vagrant-host ["vagrant" (env/env :houstan-hostname) 22]] 
   (as-> yinv/empty-inventory $
-    (yinv/target-> $ vagrant-host {"ansible_user" "vagrant"
-                                   "ansible_host" (env/env :houstan-hostname)})
-    (yinv/target->group-> $ vagrant-host "vagrant-hosts")
-    (yinv/target->group-> $ vagrant-host "java-machines")
-    (yinv/inv->ansible--list $)
+    (yinv/add-target $ vagrant-host {"ansible_user" "vagrant"
+                                     "ansible_host" (env/env :houstan-hostname)})
+    (yinv/add-target-to-group $ vagrant-host "vagrant-hosts")
+    (yinv/add-target-to-group $ vagrant-host "java-machines")
+    (yinv/make-ansible-list $)
     (json/write-str $)
     (println $)))
